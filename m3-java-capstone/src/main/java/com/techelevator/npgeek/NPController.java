cfb5781b39282be06538bbd05be219e224e79480
Abby <ahardin7@kent.edu>
2018-11-15 15:51:24 -0500

    gittincommitted

--- m3-java-capstone/src/main/java/com/techelevator/npgeek/NPController.java
+++ m3-java-capstone/src/main/java/com/techelevator/npgeek/NPController.java
@@ -2,15 +2,24 @@ package com.techelevator.npgeek;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.techelevator.npgeek.model.Park;
 import com.techelevator.npgeek.model.ParkDao;
+import com.techelevator.npgeek.model.Survey;
 import com.techelevator.npgeek.model.Weather;
+
 
 @Controller
 public class NPController {
@@ -32,6 +41,33 @@ public class NPController {
 		List <Weather> parkWeatherDays = parkDao.getWeatherByParkCode(parkCode);
 		modelHolder.put("allWeather", parkWeatherDays);
 		return "detailPage";
+	}
+	
+	@RequestMapping("/survey")
+	public String showSurveyPage(ModelMap modelHolder) {
+		if( ! modelHolder.containsAttribute("Survey")) {
+			modelHolder.addAttribute("Survey", new Survey());
+		}
+		List<Park> allParks = parkDao.getAllParks();
+		modelHolder.put("allParks", allParks);
+		return "surveyPage";
+	}
+	
+	@RequestMapping(path="/survey", method=RequestMethod.POST)
+	public String submitRegisterForm(
+			@Valid @ModelAttribute("Survey") Survey registerFormValues,
+			BindingResult result,
+			RedirectAttributes flash
+	){
+		if(result.hasErrors()) {
+			flash.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "Survey", result);
+			flash.addFlashAttribute("Survey", registerFormValues);
+			return "redirect:/survey";
+		}
+		
+		flash.addFlashAttribute("message", "You have voted.");
+		
+		return "redirect:/confirmation";
 	}
 	
 	
---
+++ m3-java-capstone/src/main/java/com/techelevator/npgeek/model/Survey.java
@@ -0,0 +1,68 @@
+package com.techelevator.npgeek.model;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Survey {
+	
+	private int surveyId;
+	
+	
+	
+	@NotBlank(message="Email is required")
+	@Email(message="email must be valid")
+	private String emailAddress;
+	
+	private String state;
+	
+	@NotBlank(message="Activity level is required")
+	private String activityLevel;
+	
+	private String parkCode;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getSurveyId() {
+		return surveyId;
+	}
+	public void setSurveyId(int surveyId) {
+		this.surveyId = surveyId;
+	}
+	public String getParkCode() {
+		return parkCode;
+	}
+	public void setParkCode(String parkCode) {
+		this.parkCode = parkCode;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getActivityLevel() {
+		return activityLevel;
+	}
+	public void setActivityLevel(String activityLevel) {
+		this.activityLevel = activityLevel;
+	}
+	
+	
+
+}
